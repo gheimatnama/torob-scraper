71b94b613340e1734d3113e90fdcb1fb9cb6bdca
Mohsen yazdani <mohsenyz2000@gmail.com>
2020-06-26 01:24:33 +0430

    Marking proxy as dead if connection failed

--- torob/utils.go
+++ torob/utils.go
@@ -9,20 +9,23 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"time"
+	"torobSpider/rotator"
 )
 
 var UAs = []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36","Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
 var cookieJar, _ = cookiejar.New(nil)
 
-func getClient(useProxy bool) *http.Client {
+func getClient(useProxy bool) (*http.Client, *rotator.Proxy) {
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 		Jar: cookieJar,
 	}
 	if useProxy {
-		client.Transport = &http.Transport{Proxy: http.ProxyURL(&CurrentRuntimeInfo.ProxyRotator.GetProxySync().Url)}
+		proxy := CurrentRuntimeInfo.ProxyRotator.GetProxySync()
+		client.Transport = &http.Transport{Proxy: http.ProxyURL(&proxy.Url)}
+		return client, proxy
 	}
-	return client
+	return client, nil
 }
 
 func DeleteCookie() {
@@ -38,8 +41,12 @@ func getJson(url string, target interface{}) error {
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", UAs[rand.Intn(3)])
-	r, err := getClient(false).Do(req)
+	client, proxy := getClient(false)
+	r, err := client.Do(req)
 	if err != nil {
+		if proxy != nil {
+			proxy.MarkDead()
+		}
 		return err
 	}
 	defer r.Body.Close()
@@ -90,13 +97,20 @@ func getText(url string) (string, error) {
 	time.Sleep((time.Duration(rand.Intn(5)) + 2) * time.Second)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", UAs[rand.Intn(3)])
-	r, err := getClient(true).Do(req)
+	client, proxy := getClient(true)
+	r, err := client.Do(req)
 	if err != nil {
+		if proxy != nil {
+			proxy.MarkDead()
+		}
 		return "", err
 	}
 	defer r.Body.Close()
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		if proxy != nil {
+			proxy.MarkDead()
+		}
 		return "", err
 	}
 	return string(data), nil
@@ -106,13 +120,20 @@ func getFakeText(url string) (string, error) {
 	time.Sleep((time.Duration(rand.Intn(7)) + 2) * time.Second)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", UAs[rand.Intn(3)])
-	r, err := getClient(true).Do(req)
+	client, proxy := getClient(true)
+	r, err := client.Do(req)
 	if err != nil {
+		if proxy != nil {
+			proxy.MarkDead()
+		}
 		return "", err
 	}
 	defer r.Body.Close()
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		if proxy != nil {
+			proxy.MarkDead()
+		}
 		return "", err
 	}
 	return string(data), nil
