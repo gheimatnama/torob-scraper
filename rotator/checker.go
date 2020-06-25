010f27b2f53143723b0e5fc51b656bee2e987d85
Mohsen yazdani <mohsenyz2000@gmail.com>
2020-06-26 01:57:26 +0430

    Increased pool training timeout

--- rotator/checker.go
+++ rotator/checker.go
@@ -26,7 +26,7 @@ func (proxyChecker *ProxyChecker) IsProxyWorking(url url.URL) bool {
 
 
 func RecaptchaChecker(proxy url.URL) bool {
-	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(&proxy),DisableKeepAlives: true}, Timeout: time.Second * 10}
+	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(&proxy),DisableKeepAlives: true}, Timeout: time.Second * 5}
 	resp, err := myClient.Get("https://api.torob.com/v4/product-page/redirect/?prk=5ced2835-f5a2-4fe5-9236-993a5cb04fd5&source=next&uid=&discover_method=search&_bt__experiment=members_clicks_04")
 	if err != nil {
 		return false
