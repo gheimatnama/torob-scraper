3b129faab2dd661adea110031a2aa02782996c4f
Mohsen yazdani <mohsenyz2000@gmail.com>
2020-06-30 00:19:05 +0430

    Changed max paraller search

--- torob/config.go
+++ torob/config.go
@@ -3,7 +3,7 @@ package torob
 var CurrentRuntimeInfo = RuntimeInfo{
 	MaxRunningWorkers: 1,
 	SearchResultLimit: 3,
-	MaxParallelSearch: make(chan int, 1),
+	MaxParallelSearch: make(chan int, 2),
 	MaxParallelProductPerSearch: make(chan int, 5),
 	WorkerPool: make(chan int, 1),
 }
